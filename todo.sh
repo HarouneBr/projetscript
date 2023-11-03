f610f39be708855db15c58d232cb628553a249e4
HarouneBr <harounemailpro@gmail.com>
2023-11-03 10:25:03 +0100

    test quitter la boucle

--- todo.sh
+++ todo.sh
@@ -1,14 +1,15 @@
    #"true" est vrai par défaut, on boucle infiniment sauf si "break"
 while true; do
 #affichage du menu et récupération du choix utilisateur
-    echo "To do list"
-    echo "1 - Ajoute une tâche"
-    echo "2 - Supprime une tâche"
-   
-    echo "To do list"
+    echo "------------------------------------------"
+    echo "---------------To do list-----------------"    
     echo "1 - Ajoute une tâche"
     echo "2 - Supprime une tâche"
     echo "3 - Affiche les tâches"
     echo -n "Choisis la prochaine action :"
+    echo "4 Quitter la boucle"
+    
 
 done
+
+
