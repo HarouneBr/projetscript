fb7043265b1f8ca9684c92c82555b2935a95570e
HarouneBr <harounemailpro@gmail.com>
2023-11-03 10:09:16 +0100

    modif2

--- todo.sh
+++ todo.sh
@@ -1,5 +1,14 @@
+   #"true" est vrai par défaut, on boucle infiniment sauf si "break"
+while true; do
+#affichage du menu et récupération du choix utilisateur
+    echo "To do list"
+    echo "1 - Ajoute une tâche"
+    echo "2 - Supprime une tâche"
+   
     echo "To do list"
     echo "1 - Ajoute une tâche"
     echo "2 - Supprime une tâche"
     echo "3 - Affiche les tâches"
-    echo -n "Choisis la prochaine action : "
+    echo -n "Choisis la prochaine action :"
+
+done
