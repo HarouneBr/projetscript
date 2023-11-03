0cfbdcd3072e04315163963b89125cf184161f29
Fouad <fouad@molengeek.com>
2023-11-03 14:21:12 +0100

    correction finale

--- todo.sh
+++ todo.sh
@@ -1,18 +1,3 @@
-   #"true" est vrai par défaut, on boucle infiniment sauf si "break"
-while true; do
-#affichage du menu et récupération du choix utilisateur
-    echo "------------------------------------------"
-    echo "---------------To do list-----------------"    
-    echo "1 - Ajoute une tâche"
-    echo "2 - Supprime une tâche"
-    echo "3 - Affiche les tâches"
-    echo -n "Choisis la prochaine action :"
-    echo "4 Quitter la boucle"
-    
-
-done
-
-
  #"true" est vrai par défaut, on boucle infiniment sauf si "break"
 while true; do
 #affichage du menu et récupération du choix utilisateur 
